4ea36478fd0abbeeee7ca4e1e3615bc0439fb1f7
Kyle Phillips <khillips375@yahoo.com>
2019-08-30 12:28:12 -0400

    Update RootValue resolver

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const {
     buildSchema
 } = require('graphql');
 const app = express();
-
 app.use(express.json());
-
+const blogs = [];
 
 app.use('/graphql', graphqlHttp({
     // String! <- exclamation point means it will always return list of string 
@@ -14,12 +13,27 @@ app.use('/graphql', graphqlHttp({
     // Name RootQuery how you name properties ie. not getblogs
     // Name Rootmutation as you name a function
     schema: buildSchema(`
+        type Blog {
+            _id: ID!
+            title: String!
+            description: String!
+            date: String!
+            rating: Float!
+        }
+
+        input BlogInput {
+            title: String!
+            description: String!
+            date: String!
+            rating:Float!
+        }
+
         type RootQuery {
-            blogs: [String!]!
+            blogs: [Blog!]!
         }
 
         type RootMutation {
-            createBlogs(title:String): String
+            createBlogs(blogInput: BlogInput): Blog
         }
 
         schema {
@@ -27,13 +41,21 @@ app.use('/graphql', graphqlHttp({
             mutation: RootMutation
         } 
     `),
+    // Resolver
     rootValue: {
         blogs: () => {
-            return ['Outdoors', 'Late Night Coding Sess', 'Gaming', 'Sports', 'Cryptocurrency'];
+            return blogs;
         },
         createBlogs: (args) => {
-            const blogTitle = args.title;
-            return blogTitle;
+            const blog = {
+                _id: Math.random().toString(),
+                title: args.blogInput.title,
+                description: args.blogInput.description,
+                date: args.blogInput.date,
+                rating: args.blogInput.rating
+            };
+            blogs.push(blog);
+            return blog;
         }
     },
     graphiql: true
