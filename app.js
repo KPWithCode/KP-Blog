090fe3eb1eb44d19fd4f41dcdcb7cde7840315d9
Kyle Phillips <khillips375@yahoo.com>
2019-08-29 22:33:43 -0400

    Building schema

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
-
+const graphqlHttp = require('express-graphql');
+const { buildSchema }  = require('graphql')
 const app = express();
 
 app.use(express.json())
 
 
 
-app.get('/', (req,res,next) => {
-    res.send(' POPPIN')
-})
+app.get('/graphql', graphqlHttp({
+schema: buildSchema(`
+    type: RootQuery {
+
+    }
+
+    type RootMutation {
+        
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation.
+    }
+`),
+rootValue:{}
+}));
+
 app.listen(3000);
