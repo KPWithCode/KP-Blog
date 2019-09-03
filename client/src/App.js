33d3b6ef43f56a27e6dc15d09a491e97a1c161ad
Kyle Phillips <khillips375@yahoo.com>
2019-09-03 19:30:46 -0400

    Added redirect, React.Fragment as well as main div for semantics

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { BrowserRouter,Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
 import AuthPage from './pages/Auth'
 import BlogPage from './pages/Blogs';
 import Highlight from './pages/Highlights';
+import Nav from './components/Navigation/Navbar';
 
 function App() {
+  
   return (
     <BrowserRouter>
-    <Switch>
-    <Redirect exact from ="/" to="/auth" />
-    <Route exact path ="/auth" component={AuthPage} />
-    <Route exact path ="/blogs" component={BlogPage} />
-    <Route exact path ="/highlights" component={Highlight} />
-    </Switch>
+      <React.Fragment>
+        <Nav />
+        <main>
+          <Switch>
+            <Redirect exact from="/" to="/auth" />
+            <Route exact path="/auth" component={AuthPage} />
+            <Route exact path="/blogs" component={BlogPage} />
+            <Route exact path="/highlights" component={Highlight} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
-      
-    
+
+
   );
 }
 
