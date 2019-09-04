4148bb5f0f9fbdcdb30a79dc1fb75be40a1e7415
Kyle Phillips <khillips375@yahoo.com>
2019-09-04 01:38:14 -0400

    Changed server to 8000

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose
     .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@blogcluster-1pxfe.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
-        app.listen(3000);
+        app.listen(8000);
     })
     .catch(err => {
         console.log(err);
