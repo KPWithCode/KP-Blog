c0e98d0023703f09d056ef98162aebdc801a82bb
Kyle Phillips <khillips375@yahoo.com>
2019-09-05 12:34:51 -0400

    Created headers that will allows my server to accept cross origin requests

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 app.use(express.json());
+// Allows my server to accept Cross origin requests
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
 app.use(isAuth);
+
 app.use('/graphql', graphqlHttp({
     // String! <- exclamation point means it will always return list of string 
     // NEVER NULL
