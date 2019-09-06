5853f721e3043549c3c4c3dd3571d6f067ee841c
Kyle Phillips <khillips375@yahoo.com>
2019-09-06 14:17:38 -0400

    Fixed syntax error

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+import AuthContext from '../../Context/auth-context';
 
-
-
-const Nav = props => {
-
-
-    return (
-        <div>
-
-        <nav className="main-nav ">
-            <div className="main-nav-logo">
-                <h1><NavLink to="/">BlogEasy</NavLink></h1>
-            </div>
-            <nav className="main-nav-items">
-                <ul>
-                    <li><NavLink to="/auth">Authenticate</NavLink></li>
-                    <li><NavLink to="/blogs">Check Out Blogs</NavLink></li>
-                    <li><NavLink to="/highlights">Highlighted</NavLink></li>
-                </ul>
-            </nav>
-        </nav>
-        <hr style={{width:'90%'}}/>
-        </div>
-    )
-}
+const Nav = props => (
+    <AuthContext.Consumer>
+        {context => {
+            return (
+                <div>
+                    <nav className="main-nav ">
+                        <div className="main-nav-logo">
+                            <h1><NavLink to="/">BlogEasy</NavLink></h1>
+                        </div>
+                        <nav className="main-nav-items">
+                            <ul>
+                                {!context.token && (<li><NavLink to="/auth">Authenticate</NavLink></li>)}
+                                <li><NavLink to="/blogs">Check Out Blogs</NavLink></li>
+                                {context.token && (<li><NavLink to="/highlights">Highlighted</NavLink></li>)}
+                            </ul>
+                        </nav>
+                    </nav>
+                    <hr style={{ width: '90%' }} />
+                </div>
+            )
+        }}
+    </AuthContext.Consumer>
+)
 export default Nav;
