1b4d2c547ddba773c438a0b962ef99774a4aa503
Kyle Phillips <khillips375@yahoo.com>
2019-09-06 14:11:35 -0400

    Added routing redirection logic for authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
 import AuthPage from './pages/Auth'
 import BlogPage from './pages/Blogs';
 import Highlight from './pages/Highlights';
 import Nav from './components/Navigation/Navbar';
+import AuthContext from './Context/auth-context';
 
-function App() {
-  
-  return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Nav />
-        <main>
-          <Switch>
-            <Redirect exact from="/" to="/auth" />
-            <Route exact path="/auth" component={AuthPage} />
-            <Route exact path="/blogs" component={BlogPage} />
-            <Route exact path="/highlights" component={Highlight} />
-          </Switch>
-        </main>
-      </React.Fragment>
-    </BrowserRouter>
+class App extends Component {
+
+  state = {
+    token: null,
+    userId: null,
+
+  }
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token, userId })
+  }
+  logout = () => {
+    this.setState({ token: null, userId: null })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <AuthContext.Provider value={{ token: this.state.token, userId: this.state.userId, login: this.login, logout: this.logout }}>
+            <Nav />
+            <main>
+              <Switch>
+                {!this.state.token && <Redirect from="/" to="/auth" exact />}
+                {this.state.token && <Redirect from="/" to="/blogs" exact />}
+                {this.state.token && <Redirect from="/auth" to="/blogs" exact />}
+                {!this.state.token && (<Route exact path="/auth" component={AuthPage} />)}
+                <Route exact path="/blogs" component={BlogPage} />
+                {this.state.token && (<Route exact path="/highlights" component={Highlight} />)}
+              </Switch>
+            </main>
+          </AuthContext.Provider>
+        </React.Fragment>
+      </BrowserRouter>
 
 
-  );
+    );
+  }
 }
 
 export default App;
