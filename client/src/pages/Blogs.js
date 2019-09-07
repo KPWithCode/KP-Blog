0068d97b7fc4d22599136cb7e3f52980c69e4913
Kyle Phillips <khillips375@yahoo.com>
2019-09-06 23:03:31 -0400

    Added on click handlers, cancel and confirm funcationality

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
-
+import './Blogs.css'
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/BackDrop/Backdrop';
 
 class BlogPage extends Component {
-
-
-
-
+    state = {
+        creating: false
+    }
+    createBlog = () => {
+        this.setState({ creating: true });
+    }
+    cancelModal = () => {
+        this.setState({ creating: false });
+    }
+    confirmModal = () => {
+        this.setState({ creating: false });
+    }
     render() {
-        return(
-                <h1>Blog Page</h1>
+        return (
+            <React.Fragment>
+                {this.state.creating &&<Backdrop />}
+                {this.state.creating && <Modal className="modal"
+                    title="Add Blog"
+                    canCancel
+                    canConfirm
+                    onCancel={this.cancelModal}
+                    onConfirm={this.confirmModal}
+                >
+                    <p>MODAL CONTENT</p>
+                </Modal>}
+                <div>
+                    <div className="header">
+                        <h1 className="h1">SHARE YOUR BLOG</h1>
+                        <button className="btn" onClick={this.createBlog} >Start Here</button>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
